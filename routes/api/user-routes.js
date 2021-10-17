296d9b93c35e3bd60b949602cf44588e136ae121
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-10-17 16:42:46 -0700

    added search for pass through bcrypt

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
     //Access our User model and run .findAll() method
     User.findAll({
         attributes: { exclude: ['password'] }
-        })
+    })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
@@ -50,6 +50,30 @@ router.post('/', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
@@ -78,19 +102,19 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
     User.destroy({
         where: {
-          id: req.params.id
+            id: req.params.id
         }
-      })
+    })
         .then(dbUserData => {
-          if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id' });
-            return;
-          }
-          res.json(dbUserData);
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
         })
         .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
+            console.log(err);
+            res.status(500).json(err);
         });
 });
 
